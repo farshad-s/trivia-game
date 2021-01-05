9378d727bf601f2532053ebf5baaaa3c390dfe43
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-05 22:59:48 +0000

    added endpoint + fetch api with a console log to test

--- src/app/accessApi/accessApi.js
+++ src/app/accessApi/accessApi.js
@@ -1,1 +1,13 @@
-export const accessApi = () => {};
+let endpoint =
+  "https://opentdb.com/api.php?amount=30&category=9&difficulty=easy&type=multiple";
+
+export const accessApi = () => {
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("error is", error);
+    });
+};
