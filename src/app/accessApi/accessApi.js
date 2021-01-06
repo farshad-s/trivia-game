bae295a5a96a0e6c8c7426afb19bf666eb66b8a5
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-06 18:44:32 +0000

    accessing answers + testing with console.log

--- src/app/accessApi/accessApi.js
+++ src/app/accessApi/accessApi.js
@@ -7,6 +7,7 @@ let answerOne = document.getElementById("answer-1")
 let answerTwo = document.getElementById("answer-2")
 let answerThree = document.getElementById("answer-3")
 let answerFour = document.getElementById("answer-4")
+let answers = document.getElementsByName("answer")
 
 export const accessApi = () => {
   fetch(endpoint)
@@ -14,12 +15,16 @@ export const accessApi = () => {
     .then((data) => {
       let num = 0;
       nextQuestion.addEventListener("click", function() {
+        console.log(answerOne)
         question.innerText = data.results[num].question
         answerOne.innerText = data.results[num].incorrect_answers[0]
         answerTwo.innerText = data.results[num].incorrect_answers[1]
         answerThree.innerText = data.results[num].correct_answer
         answerFour.innerText = data.results[num].incorrect_answers[2]
         num++;
+        for (let i = 0; i < answers.length; i++) {
+          console.log(answers[i].innerText)
+        }
       })
     })
     .catch((error) => {
