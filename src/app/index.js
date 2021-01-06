3d0172f784b7f1c014e7a744c17a21c53a4ac4fb
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-06 18:31:38 +0000

    removed function related to submit button

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,10 @@
 import { accessApi } from "./accessApi";
-import { submitAnswer } from "./submitButton"
 import { displayAfterStart } from "./display"
 
 import "@styles/main.scss";
 
 const App = () => {
   accessApi();
-  submitAnswer();
   displayAfterStart();
 };
 
