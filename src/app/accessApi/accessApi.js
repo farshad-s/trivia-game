afe99f7d42a947e82d81553cbea5d4602b077fa9
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-06 13:19:18 +0000

    added next question button + event listener so when it is clicked the question changes

--- src/app/accessApi/accessApi.js
+++ src/app/accessApi/accessApi.js
@@ -2,13 +2,17 @@ let endpoint =
   "https://opentdb.com/api.php?amount=30&category=9&difficulty=easy&type=multiple";
 
 let question = document.getElementById("question")
+let nextQuestion = document.getElementById("next-question")
 
 export const accessApi = () => {
   fetch(endpoint)
     .then((response) => response.json())
     .then((data) => {
       let num = 0;
-      question.innerText = data.results[num].question
+      nextQuestion.addEventListener("click", function() {
+        question.innerText = data.results[num].question  
+        num++;
+      })
     })
     .catch((error) => {
       console.log("error is", error);
