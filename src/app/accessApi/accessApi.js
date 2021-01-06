085170c2b480bb2213333f56cd1ea3885eda21e3
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-06 13:10:06 +0000

    accessed question from html file & updated to become the question from api

--- src/app/accessApi/accessApi.js
+++ src/app/accessApi/accessApi.js
@@ -1,11 +1,13 @@
 let endpoint =
   "https://opentdb.com/api.php?amount=30&category=9&difficulty=easy&type=multiple";
 
+let question = document.getElementById("question")
+
 export const accessApi = () => {
   fetch(endpoint)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      question.innerText = data.results[0].question
     })
     .catch((error) => {
       console.log("error is", error);
