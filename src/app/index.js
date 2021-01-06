389e0d37510782f2fa6a53d4e1d6f99075b37277
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-06 19:32:19 +0000

    imported start quiz function

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,13 @@
 import { accessApi } from "./accessApi";
-import { displayAfterStart } from "./display"
+import { displayAfterStart } from "./display";
+import { start } from "./startQuiz";
 
 import "@styles/main.scss";
 
 const App = () => {
   accessApi();
   displayAfterStart();
+  start();
 };
 
 App();
