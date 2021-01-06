1f7fc8ac23ede7f38f470dfc0511baa386c0aace
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-06 17:04:02 +0000

    imported displayAfterStart function

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,13 @@
 import { accessApi } from "./accessApi";
 import { submitAnswer } from "./submitButton"
+import { displayAfterStart } from "./display"
 
 import "@styles/main.scss";
 
 const App = () => {
   accessApi();
   submitAnswer();
+  displayAfterStart();
 };
 
 App();
