0fc1eb866a4c923ef813527788713acb909f53d9
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-06 16:57:34 +0000

    imported submitAnswer function and placed it in App function

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,11 @@
 import { accessApi } from "./accessApi";
+import { submitAnswer } from "./submitButton"
 
 import "@styles/main.scss";
 
 const App = () => {
   accessApi();
+  submitAnswer();
 };
 
 App();
